18b6dedb479fa4511c797eed40e6b1efc7920507
Alexandr Morohin <finersanya@gmail.com>
2022-10-25 15:13:03 +0500

    Переход с pygame на tkinter

--- main.py
+++ main.py
@@ -1,205 +1,37 @@
-import math
-import numpy
-
-from utils import *
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("pyPaint")
+from tkinter import *
+from tkinter.messagebox import *
+from src.paint import *
 
 
-def draw(win, cursorPoint):
-    win.fill(BG_COLOR)
-    draw_lines()
-
-    if currentAction == ACTION_DRAW:
-        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-    elif currentAction == ACTION_MOVE_POINT:
-        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-    elif currentAction == ACTION_NO:
-        if cursorOnLineEnd(cursor, radius=5) is not None:
-            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_SIZEWE)
-        elif cursorOnLine(cursorPoint, zoneRadius=20) is not None:
-            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_SIZEALL)
-        else:
-            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-
-    pygame.display.update()
+# выход из программы
+def close_win():
+    if askyesno("Выход", "Вы уверены?"):
+        root.destroy()
 
 
-def draw_lines():
-    for i in range(len(points)):
-        if pointId is not None:
-            if currentAction == ACTION_MOVE_POINT and i == pointId[0]:
-                continue
-
-            if currentAction == ACTION_MOVE_LINE and i == pointId:
-                continue
-
-        x1 = points[i][0][0]
-        x2 = points[i][1][0]
-        y1 = points[i][0][1]
-        y2 = points[i][1][1]
-
-        A = x2 - x1
-        B = y2 - y1
-        C = x1 * y2 - x2 * y1
-
-        B *= -1
-
-        font = get_font(22)
-        text_surface = font.render(f"{A}x + {B}y + {C}", 1, BLACK)
-        centerLine = (
-            ((x1 + x2) / 2),
-            ((y1 + y2) / 2),
-        )
-        WIN.blit(text_surface, centerLine)
-        pygame.draw.line(WIN, BLACK, (x1, y1), (x2, y2))
+# вывод справки
+def about():
+    showinfo("Контакты", "vk.com/alexfiner")
 
 
-def cursorOnLine(cursorPoint, zoneRadius=3):
-    # credits: https://clck.ru/R9STG
-    # def dist(x1_, x2_, y1_, y2_):
-    #     return math.sqrt((x2_ - x1_) ** 2 + (y2_ - y1_) ** 2)
-    #
-    # def dot(x1_, x2_, y1_, y2_):
-    #     return (x1_ * x2_) + (y1_ * y2_)
-    #
-    # def vector(x1_, x2_, y1_, y2_):
-    #     distance = [x1 - x2, y1 - y2]
-    #     norm = math.sqrt(distance[0] ** 2 + distance[1] ** 2)
-    #     direction = [distance[0] / norm, distance[1] / norm]
-    #     return direction
+# функция для создания главного окна
+def main():
+    global root
+    root = Tk()
+    root.geometry("800x600+300+300")
+    app = Paint(root)
+    m = Menu(root)
+    root.config(menu=m)
 
-    for i in range(len(points)):
-        x1 = points[i][0][0]
-        x2 = points[i][1][0]
-        y1 = points[i][0][1]
-        y2 = points[i][1][1]
+    fm = Menu(m)
+    m.add_cascade(label="Файл", menu=fm)
+    fm.add_command(label="Выход", command=close_win)
 
-        x = cursorPoint[0]
-        y = cursorPoint[1]
-
-        yTop = max(y1, y2)
-        yBottom = min(y1, y2)
-
-        yTop += 20
-        yBottom -= 20
-
-        if y < yBottom or y > yTop:
-            continue
-
-        # (x - x1)/(x2 - x1) = (y - y1)/(y2 - y1)
-        xCord = ((y - y1)*(x2 - x1))/(y2 - y1) + x1
-        distToLine = abs(x - xCord)
-        if distToLine > 20:
-            continue
-
-        print(distToLine)
-        return i
-
-    return None
+    hm = Menu(m)
+    m.add_cascade(label="Справка", menu=hm)
+    hm.add_command(label="О программе", command=about)
+    root.mainloop()
 
 
-def cursorOnLineEnd(cursorPoint, radius=5):
-    for i in range(len(points)):
-        if (pow((cursorPoint[0] - points[i][0][0]), 2) + pow((cursorPoint[1] - points[i][0][1]), 2)) <= pow(radius, 2):
-            return i, 0
-
-        if (pow((cursorPoint[0] - points[i][1][0]), 2) + pow((cursorPoint[1] - points[i][1][1]), 2)) <= pow(radius, 2):
-            return i, 1
-
-    return None
-
-
-run = True
-clock = pygame.time.Clock()
-points = []
-currentAction = ACTION_NO
-posStart = None
-pointId = None
-positionMovePoint = None
-hoverLineId = None
-
-while run:
-    # Ограничение FPS
-    clock.tick(FPS)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    cursor = pygame.mouse.get_pos()
-    draw(WIN, cursor)
-
-    # Нажата ЛЕВАЯ кнопка мыши
-
-    if currentAction == ACTION_NO:
-        lineId = cursorOnLine(cursor, zoneRadius=3)
-        # if lineId is not None:
-        hoverLineId = lineId
-
-    if pygame.mouse.get_pressed()[0]:
-        # ЛКМ ЗАЖАТА
-        if currentAction == ACTION_NO:
-            pointId = cursorOnLineEnd(cursor, radius=5)
-            if pointId is not None:
-                currentAction = ACTION_MOVE_POINT
-            else:
-                if hoverLineId is not None:
-                    posStart = cursor
-                    pointId = hoverLineId
-                    currentAction = ACTION_MOVE_LINE
-                else:
-                    posStart = cursor
-                    currentAction = ACTION_DRAW
-
-
-    if not pygame.mouse.get_pressed()[0]:
-        if currentAction == ACTION_DRAW:
-            # Это не точка!
-            if posStart != cursor:
-                points.append((posStart, cursor))
-
-        if currentAction == ACTION_MOVE_POINT:
-            if positionMovePoint is not None and pointId is not None:
-                points[pointId[0]] = positionMovePoint
-
-        if currentAction == ACTION_MOVE_LINE:
-            if hoverLineId is not None:
-                moved = (cursor[0]-posStart[0], cursor[1]-posStart[1])
-                points[hoverLineId] = positionMovePoint
-
-        currentAction = ACTION_NO
-
-    # Нажата средняя кнопка мыши
-    if pygame.mouse.get_pressed()[1]:
-        # Средняя кнопка мыши
-        if currentAction == ACTION_NO:
-            # Удаление
-            if hoverLineId is not None:
-                points.pop(hoverLineId)
-                # print('Удаление')
-
-    if currentAction == ACTION_DRAW and posStart is not None:
-        pygame.draw.line(WIN, RED, (posStart[0], posStart[1]), (cursor[0], cursor[1]))
-
-    if currentAction == ACTION_MOVE_LINE and hoverLineId is not None and posStart is not None:
-        moved = (cursor[0]-posStart[0], cursor[1]-posStart[1])
-
-        positionMovePoint = (
-            (points[hoverLineId][0][0] + moved[0], points[hoverLineId][0][1] + moved[1]),
-            (points[hoverLineId][1][0] + moved[0], points[hoverLineId][1][1] + moved[1]),
-        )
-        pygame.draw.line(WIN, RED, positionMovePoint[0], positionMovePoint[1])
-
-    if currentAction == ACTION_MOVE_POINT and pointId is not None:
-        positionMovePoint = (
-            (points[pointId[0]][0][0], points[pointId[0]][0][1]), (cursor[0], cursor[1]),
-        ) if pointId[1] == 1 else (
-            (points[pointId[0]][1][0], points[pointId[0]][1][1]), (cursor[0], cursor[1]),
-        )
-        pygame.draw.line(WIN, RED, positionMovePoint[0], positionMovePoint[1])
-
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
---
+++ src/paint.py
@@ -0,0 +1,207 @@
+from tkinter import *
+from tkinter import colorchooser
+from utils import *
+
+
+class Paint(Frame):
+    def __init__(self, parent):
+        super().__init__()
+        self.canv = None
+        self.parent = parent
+        self.brush_size = 2
+        self.color = "red"
+        self.cursor_start = (0, 0)
+        self.current_line = None
+        self.lines = []
+        self.buttons_color = []
+        self.buttons_size = []
+        self.buttons_mode = []
+        self.current_action = EVENT_NONE
+        self.line_hovered = None
+        self.line_coords = (0, 0, 0, 0)
+        self.current_mode = MODE_MAKE_LINES
+        self.setUI()
+
+    def line_hover(self, event):
+        print(event)
+
+        if self.current_mode == MODE_MOVE_LINES:
+            if self.current_action == EVENT_NONE:
+                x, y = event.x, event.y
+                self.cursor_start = (x, y)
+                self.current_line = event.widget.find_withtag('current')[0]
+                self.line_coords = self.canv.coords(self.current_line)
+                self.current_action = EVENT_MOVE_LINE
+
+        if self.current_mode == MODE_DELETE_LINES:
+            self.current_line = event.widget.find_withtag('current')[0]
+            self.canv.delete(self.current_line)
+
+    def draw_line_start(self, event):
+        if self.current_mode == MODE_MAKE_LINES:
+            if self.current_action == EVENT_NONE:
+                self.current_action = EVENT_DRAW_LINE
+                x, y = event.x, event.y
+                self.cursor_start = (x, y)
+                self.current_line = self.canv.create_line(x, y, x, y, fill=self.color, width=self.brush_size)
+                self.canv.tag_bind(self.current_line, '<Button-1>', self.line_hover)
+
+        if self.current_mode == MODE_MOVE_LINES:
+            pass
+
+    def draw_line_action(self, event):
+        if self.current_mode == MODE_MAKE_LINES:
+            if self.current_action == EVENT_DRAW_LINE:
+                x1, y1 = self.cursor_start
+                x, y = event.x, event.y
+                self.canv.coords(self.current_line, x1, y1, x, y)
+
+        if self.current_mode == MODE_MOVE_LINES:
+            if self.current_action == EVENT_MOVE_LINE:
+                x1, y1 = self.cursor_start
+                x, y = event.x, event.y
+
+                move = x-x1, y-y1
+
+                self.canv.coords(self.current_line,
+                                 self.line_coords[0] + move[0],
+                                 self.line_coords[1] + move[1],
+                                 self.line_coords[2] + move[0],
+                                 self.line_coords[3] + move[1]
+                                 )
+
+    def draw_line_end(self, event):
+        if self.current_mode == MODE_MAKE_LINES:
+            if self.current_action == EVENT_DRAW_LINE:
+                self.current_action = EVENT_NONE
+                x1, y1 = self.cursor_start
+                x, y = event.x, event.y
+                self.canv.coords(self.current_line, x1, y1, x, y)
+
+        if self.current_mode == MODE_MOVE_LINES:
+            if self.current_action == EVENT_MOVE_LINE:
+                self.current_action = EVENT_NONE
+                x1, y1 = self.cursor_start
+                x, y = event.x, event.y
+
+                move = x-x1, y-y1
+
+                self.canv.coords(self.current_line,
+                                 self.line_coords[0] + move[0],
+                                 self.line_coords[1] + move[1],
+                                 self.line_coords[2] + move[0],
+                                 self.line_coords[3] + move[1]
+                                 )
+
+
+    def set_color(self, new_color, button):
+        self.color = new_color
+        for btn in self.buttons_color:
+            btn['state'] = NORMAL
+        button['state'] = DISABLED
+
+    def set_color_picker(self):
+        color_code = colorchooser.askcolor(title="Выбор цвета")
+        self.color = color_code[1]
+        for btn in self.buttons_color:
+            btn['state'] = NORMAL
+
+    def set_brush_size(self, new_size, button):
+        self.brush_size = new_size
+        for btn in self.buttons_size:
+            btn['state'] = NORMAL
+        button['state'] = DISABLED
+
+    def set_mode(self, mode, button):
+        self.current_mode = mode
+        for btn in self.buttons_mode:
+            btn['state'] = NORMAL
+        button['state'] = DISABLED
+
+    def clear(self):
+        self.canv.delete("all")
+        self.lines = []
+
+    def setUI(self):
+        self.parent.title("PyGame")
+        self.pack(fill=BOTH, expand=1)
+
+        self.columnconfigure(6, weight=1)
+        self.rowconfigure(3, weight=1)
+
+        # Создаем холст с белым фоном
+        self.canv = Canvas(self, bg="white")
+        self.canv.grid(row=3, column=0, columnspan=7, padx=5, pady=5, sticky=E + W + S + N)
+
+        self.canv.bind("<ButtonPress-1>", self.draw_line_start)
+        self.canv.bind("<B1-Motion>", self.draw_line_action)
+        self.canv.bind("<ButtonRelease-1>", self.draw_line_end)
+
+        # создаем метку для кнопок изменения цвета кисти
+        color_lab = Label(self, text="Цвет: ")
+        color_lab.grid(row=0, column=0, padx=6)
+
+        red_btn = Button(self, text="Красный", width=10, command=lambda: self.set_color("red", red_btn))
+        red_btn.grid(row=0, column=1)
+        red_btn['state'] = DISABLED
+        self.buttons_color.append(red_btn)
+
+        green_btn = Button(self, text="Зеленый", width=10, command=lambda: self.set_color("green", green_btn))
+        green_btn.grid(row=0, column=2)
+        self.buttons_color.append(green_btn)
+
+        blue_btn = Button(self, text="Синий", width=10, command=lambda: self.set_color("blue", blue_btn))
+        blue_btn.grid(row=0, column=3)
+        self.buttons_color.append(blue_btn)
+
+        black_btn = Button(self, text="черный", width=10, command=lambda: self.set_color("black", black_btn))
+        black_btn.grid(row=0, column=4)
+        self.buttons_color.append(black_btn)
+
+        my_color_btn = Button(self, text="Свой цвет", width=10, command=self.set_color_picker)
+        my_color_btn.grid(row=0, column=5)
+
+        clear_btn = Button(self, text="Очистить", width=10, command=lambda: self.canv.delete("all"))
+        clear_btn.grid(row=0, column=6, sticky=W)
+
+        size_lab = Label(self, text="Размер линии: ")
+        size_lab.grid(row=1, column=0, padx=5)
+        one_btn = Button(self, text="2x", width=10, command=lambda: self.set_brush_size(2, one_btn))
+        one_btn.grid(row=1, column=1)
+        one_btn['state'] = DISABLED
+        self.buttons_size.append(one_btn)
+
+        two_btn = Button(self, text="5x", width=10, command=lambda: self.set_brush_size(5, two_btn))
+        two_btn.grid(row=1, column=2)
+        self.buttons_size.append(two_btn)
+
+        five_btn = Button(self, text="7x", width=10, command=lambda: self.set_brush_size(7, five_btn))
+        five_btn.grid(row=1, column=3)
+        self.buttons_size.append(five_btn)
+
+        seven_btn = Button(self, text="10x", width=10, command=lambda: self.set_brush_size(10, seven_btn))
+        seven_btn.grid(row=1, column=4)
+        self.buttons_size.append(seven_btn)
+
+        ten_btn = Button(self, text="20x", width=10, command=lambda: self.set_brush_size(20, ten_btn))
+        ten_btn.grid(row=1, column=5)
+        self.buttons_size.append(ten_btn)
+
+        twenty_btn = Button(self, text="50x", width=10, command=lambda: self.set_brush_size(50, twenty_btn))
+        twenty_btn.grid(row=1, column=6, sticky=W)
+        self.buttons_size.append(twenty_btn)
+
+        mode_lab = Label(self, text="Режим: ")
+        mode_lab.grid(row=2, column=0, padx=0)
+        mode_draw = Button(self, text="Рисование", width=10, command=lambda: self.set_mode(MODE_MAKE_LINES, mode_draw))
+        mode_draw.grid(row=2, column=1)
+        mode_draw['state'] = DISABLED
+        self.buttons_mode.append(mode_draw)
+
+        mode_move = Button(self, text="Перемещение", width=10, command=lambda: self.set_mode(MODE_MOVE_LINES, mode_move))
+        mode_move.grid(row=2, column=2)
+        self.buttons_mode.append(mode_move)
+
+        mode_del = Button(self, text="Удаление", width=10, command=lambda: self.set_mode(MODE_DELETE_LINES, mode_del))
+        mode_del.grid(row=2, column=3)
+        self.buttons_mode.append(mode_del)
