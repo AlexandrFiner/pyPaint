0d776f8d27a79756d7317593f57ae5263c8677c3
Alexandr Morohin <finersanya@gmail.com>
2022-10-28 12:57:03 +0500

    Центральная позиция

--- src/geometry.py
+++ src/geometry.py
@@ -10,32 +10,30 @@ class Geometry:
         self._zoom = 0.5
         self.CANVAS_WIDTH = canvas_width
         self.CANVAS_HEIGHT = canvas_height
-        self.OBJECT_POSITION = [canvas_width // 2, canvas_height - 20]
+        self.OBJECT_POSITION = [canvas_width // 2, canvas_height // 2 - 20]
 
     def _reset_rotation(self):
         self._angle_x = self._angle_y = self._angle_z = 0
 
     def transform_point(self, point, rotation_x, rotation_y, rotation_z):
-        print(point)
+        point[0][0] -= self.OBJECT_POSITION[0]
+        point[1][0] -= self.OBJECT_POSITION[1]
         rotated_2d = np.matmul(rotation_x, point)
         rotated_2d = np.matmul(rotation_y, rotated_2d)
         rotated_2d = np.matmul(rotation_z, rotated_2d)
 
-        print(rotated_2d)
         # z = 0.5 / (self._zoom - rotated_2d[2][0])
         z = 1
         projection_matrix = [[z, 0, 0],
                              [0, z, 0]]
-        print(projection_matrix)
+
         projected_2d = np.matmul(projection_matrix, rotated_2d)
 
-        # x = int(projected_2d[0][0]) + self.g[0]
-        # y = -int(projected_2d[1][0]) + self.OBJECT_POSITION[1]
+        x = int(projected_2d[0][0]) + self.OBJECT_POSITION[0]
+        y = int(projected_2d[1][0]) + self.OBJECT_POSITION[1]
 
-        x = int(projected_2d[0][0])
-        y = int(projected_2d[1][0])
-
-        print(projected_2d)
+        # x = int(projected_2d[0][0])
+        # y = int(projected_2d[1][0])
 
         return x, y
 
